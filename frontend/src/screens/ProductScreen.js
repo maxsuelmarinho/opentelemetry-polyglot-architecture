5c5a26a279ef5e84318538f69c11c80f2ecc6bdb
Maxsuel Marinho <maxsuelmarinho@gmail.com>
2021-02-20 15:19:17 -0300

    Add meta data

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -5,6 +5,7 @@ import { Row, Col, Image, ListGroup, Card, Button, Form } from 'react-bootstrap'
 import Rating from '../components/Rating';
 import Message from '../components/Message';
 import Loader from '../components/Loader';
+import Meta from '../components/Meta';
 import { 
     productDetailsAction,
     createProductReview,
@@ -50,6 +51,7 @@ const ProductScreen = ({ history, match }) => {
             <Link className='btn btn-light my-3' to='/'>Go Back</Link>
             { loading ? <Loader /> : error ? <Message variant='danger'>{error}</Message> : (
                 <>
+                    <Meta title={product.name} />
                     <Row>
                         <Col md={6}>
                             <Image src={product.image} alt={product.name} fluid />
