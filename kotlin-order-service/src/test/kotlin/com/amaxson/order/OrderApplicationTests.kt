0d39f2e24b799250db8877156140d13cca0e0a32
Maxsuel Marinho <maxsuelmarinho@gmail.com>
2021-03-03 21:35:01 -0300

    Add test configuration to the order service

--- kotlin-order-service/src/test/kotlin/com/amaxson/order/OrderApplicationTests.kt
+++ kotlin-order-service/src/test/kotlin/com/amaxson/order/OrderApplicationTests.kt
@@ -1,8 +1,10 @@
 package com.amaxson.order
 
 import org.junit.jupiter.api.Test
+import org.springframework.test.context.ActiveProfiles
 import org.springframework.boot.test.context.SpringBootTest
 
+@ActiveProfiles("test")
 @SpringBootTest
 class OrderApplicationTests {
 
